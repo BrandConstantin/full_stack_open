1c209250c4b6386f4a769075438a4baaa16c80fa
BrandConstantin <constantin.brindu@gmail.com>
2024-03-26 18:22:33 +0100

    renderizar página, manejo eventos, pasar estado a componentes hijos

---
+++ react/02_componentes/02_re-renderizar_pagina/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+let counter = 1
+
+const refresh = () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App counter={counter} />
+    </React.StrictMode>,
+  )
+}
+
+setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000)
+setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000)
+setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000)
---
+++ react/02_componentes/03_manejo_eventos/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+
+      <button onClick={() => setCounter(0)}>
+        zero
+      </button>
+    </div>
+  )
+}
+
+export default App   
