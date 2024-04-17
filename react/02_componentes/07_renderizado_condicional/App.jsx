23a89e7d7ff275d9a12bdf8c6d87ed89fc55eaeb
BrandConstantin <constantin.brindu@gmail.com>
2024-04-17 17:51:12 +0200

    estados complejos, arrays, renderizado condicional

---
+++ react/02_componentes/07_renderizado_condicional/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        )
+    }
+    return (
+        <div>
+            button press history: {props.allClicks.join(' ')}
+        </div>
+    )
+}
+
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+        {text}
+    </button>
+)
+
+const App = () => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+    const [total, setTotal] = useState(0)
+
+    // porque React trabaja de forma asincrona
+    // actualizar datos en tiempo real
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        const updatedLeft = left + 1
+        setLeft(updatedLeft)
+        setTotal(updatedLeft + right)
+    }
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
+        setTotal(left + right)
+    }
+
+    return (
+        <div>
+            {left}
+            <Button handleClick={handleLeftClick} text='left' />
+            <Button handleClick={handleRightClick} text='right' />
+            {right}
+            <History allClicks={allClicks} />
+            <p>total {total}</p>
+        </div>
+    )
+}
+
+export default App   
